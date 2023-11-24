1ea4c3c5fa52a8f9e156d1cc39e074c27eae409d
0Franky <frhaexin@gmail.com>
2023-11-24 18:26:10 +0100

    update

--- boot-usb-passthrough-GUI/ui/components/box.py
+++ boot-usb-passthrough-GUI/ui/components/box.py
@@ -1,34 +1,42 @@
-from PyQt5.QtWidgets import QVBoxLayout, QGroupBox, QCheckBox, QLabel, QHBoxLayout
+from PyQt5.QtWidgets import QVBoxLayout, QGroupBox, QCheckBox, QLabel, QHBoxLayout, QScrollArea, QWidget
 import consts.app_settings as AppSettings
 from ui.components.flow_layout import FlowLayout
 
-def createGroupBox(title, content = [], show_switch=False, switch_title=""):
-  # Crea una box con titolo e contenuto
-  group_box = QGroupBox(title)
+def createGroupBox(title, content=[], show_switch=False, switch_title=""):
+    # Crea una box con titolo e contenuto
+    group_box = QGroupBox(title)
 
-  # Configura stile per spostare il titolo sopra il bordo
-  style_sheet = f"QGroupBox {{ border: 2px solid gray; border-radius: {AppSettings.border_radius}; margin-top: 0.5em; padding: 0.5em; }} "
-  style_sheet += f"QGroupBox:title {{ subcontrol-origin: margin; subcontrol-position: top center; padding: 0 0.5em; }}"
-  group_box.setStyleSheet(style_sheet)
+    # Configura stile per spostare il titolo sopra il bordo
+    style_sheet = f"QGroupBox {{ border: 2px solid gray; border-radius: {AppSettings.border_radius}; margin-top: 0.5em; padding: 0.5em; }} "
+    style_sheet += f"QGroupBox:title {{ subcontrol-origin: margin; subcontrol-position: top center; padding: 0 0.5em; }}"
+    group_box.setStyleSheet(style_sheet)
 
-  # Imposta il layout della box
-  box_layout = QVBoxLayout(group_box)
+    # Imposta il layout della box
+    box_layout = QVBoxLayout(group_box)
 
-  # Aggiungi uno switch opzionale
-  if show_switch:
-    switch_layout = QHBoxLayout()
-    switch_layout.addStretch()
+    # Aggiungi uno switch opzionale
+    if show_switch:
+        switch_layout = QHBoxLayout()
+        switch_layout.addStretch()
 
-    switch = QCheckBox(switch_title)
-    switch_layout.addWidget(switch)
+        switch = QCheckBox(switch_title)
+        switch_layout.addWidget(switch)
 
-    box_layout.addLayout(switch_layout)
+        box_layout.addLayout(switch_layout)
 
-  # Aggiungi il contenuto alla box
-  flow_layout = FlowLayout(hSpacing=AppSettings.margin_value, vSpacing=AppSettings.margin_value)
-  box_layout.addLayout(flow_layout)
+    # Aggiungi il contenuto alla box
+    flow_layout = FlowLayout(hSpacing=AppSettings.margin_value, vSpacing=AppSettings.margin_value)
+    box_layout.addLayout(flow_layout)
 
-  for item in content:
-    flow_layout.addWidget(item)
+    for item in content:
+        flow_layout.addWidget(item)
 
-  return group_box
+    # Wrappa la box_layout in un widget scorrevole
+    scroll_area = QScrollArea()
+    scroll_area.setWidgetResizable(True)
+    
+    scroll_content = QWidget()
+    scroll_content.setLayout(box_layout)
+    scroll_area.setWidget(scroll_content)
+
+    return scroll_area
